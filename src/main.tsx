c77a373da5c21fb51a6217adc185b3f7dca1f1e7
sxc1 <56100665+sxc1@users.noreply.github.com>
2025-08-31 22:37:59 -0700

    Initial Vite React template with MUI imports

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+import App from './App.tsx'
+
+const theme = createTheme()
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+)
